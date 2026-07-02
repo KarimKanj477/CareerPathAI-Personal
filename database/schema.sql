2558cfdc2937e14e1e4c41811debfdf907e95036
KarimKanj477 <karim.kanj05@gmail.com>
2026-07-02 12:11:32 +0300

    database: add initial schema file

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,9 @@
+CREATE DATABASE careerpath_ai;
 
+USE careerpath_ai;
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(255)
+);
